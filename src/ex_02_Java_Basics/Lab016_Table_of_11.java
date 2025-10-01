4dd547bbd252641f837bf34b33e8d978e6e6ba5d
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-01 22:26:18 +0530

    14 Sept 2025 Adding files Part 3

---
+++ src/ex_02_Java_Basics/Lab016_Table_of_11.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab016_Table_of_11 {
+
+    public static void main(String[] args) {
+        int table = 9;
+        for(int i=1;i<=10;i++)
+            System.out.printf("%d x %d = %d \n", table, i, table*i);
+    }
+
+}
