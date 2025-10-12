7ea0e0587b80b63aa195992b36d17040e34a7f47
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-12 09:55:24 +0530

    Tasks of 5 October Completed

--- src/ex_Java_Tasks/Task_5_Oct/Second_Max.java
+++ src/ex_Java_Tasks/Task_5_Oct/Second_Max.java
@@ -2,6 +2,19 @@ package ex_Java_Tasks.Task_5_Oct;
 
 public class Second_Max {
     public static void main(String[] args) {
-        int a,b,c;
+        int[] nums2 = {12,34,10,1,100,3,4,32};
+
+        int max = 0;
+        int max2 = 0;
+
+        for (int i : nums2) {
+            if (i > max) {
+                max2 = max;
+                max = i;
+            } else if (i > max2 && i != max) {
+                max2 = i;
+            }
+        }
+        System.out.println(max2);
     }
 }
--- src/ex_Java_Tasks/Task_5_Oct/String_Operations.java
+++ src/ex_Java_Tasks/Task_5_Oct/String_Operations.java
@@ -1,7 +1,53 @@
 package ex_Java_Tasks.Task_5_Oct;
 
+import java.util.Scanner;
+
 public class String_Operations {
     public static void main(String[] args) {
-        String s = "Dhaval";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String ");
+
+        String s = sc.nextLine();
+
+        String_Operations so = new String_Operations();
+
+        //Write a program to reverse a string without using inbuilt functions.
+        System.out.println(so.reverseMe(s));
+
+        //Count the Number of Words in a String.
+        System.out.println(so.numberOfWords(s));
+
+        //First Letter of Each Word in a String.
+        System.out.println(so.FirstLetterOfEachWord(s));
+
     }
+
+    String reverseMe(String str) {
+        String rs = "";
+        for (int i = str.length()-1; i >= 0 ; i--) {
+            rs = rs + str.charAt(i);
+        }
+
+        return rs;
+    }
+
+    int numberOfWords(String str) {
+
+        String[] words = str.split(" ");
+        return  words.length;
+    }
+
+    String FirstLetterOfEachWord(String str) {
+
+        String firstLetters = "";
+        String[] words = str.split(" ");
+        for (String word: words) {
+            firstLetters = firstLetters + word.charAt(0);
+        }
+        return  firstLetters;
+    }
+
+
+
+
 }
