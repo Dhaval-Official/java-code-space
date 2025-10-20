b891fcec1040204db4c8a19c70c64e76a98530d9
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-20 23:16:05 +0530

    Extra Practice Codes from sdet

--- src/ex_Java_Extras/Factorial.java
+++ src/ex_Java_Extras/Factorial.java
@@ -1,4 +1,21 @@
 package ex_Java_Extras;
 
+import java.util.Scanner;
+
 public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int fact = num;
+
+        while (num >1 ) {
+
+            num--;
+            fact*=num;
+
+        }
+
+        System.out.println(fact);
+    }
 }
