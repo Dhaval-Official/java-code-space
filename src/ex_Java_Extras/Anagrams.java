b01d7d50acab978ffefa17fa188d2983e8e18a03
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-22 21:53:43 +0530

    Extra Practice Codes from sdet

--- src/ex_Java_Extras/Anagrams.java
+++ src/ex_Java_Extras/Anagrams.java
@@ -7,6 +7,7 @@ public class Anagrams {
     public static void main(String[] args) {
         Hashtable<Character,Integer> ht = new Hashtable<Character,Integer>();
         int count;
+        boolean isAnagram = true;
 
         Scanner sc = new Scanner(System.in);
 
@@ -21,27 +22,34 @@ public class Anagrams {
 //            System.out.println(w1.charAt(i));
 
             if(ht.contains(w1.charAt(i))) {
-                count = (int) ht.get(w1.charAt(i));
+                count = ht.get(w1.charAt(i));
                 ht.put(w1.charAt(i),++count);
             } else {
                 ht.put(w1.charAt(i),1);
             }
         }
 
+        System.out.println();
 
-        for (int i = 0; i < w1.length(); i++) {
-            System.out.println(w1.charAt(i));
-
-            if(ht.contains(w1.charAt(i))) {
-                count = (int) ht.get(w1.charAt(i));
-                ht.put(w1.charAt(i),++count);
+        for (int i = 0; i < w2.length(); i++) {
+//            System.out.println(w2.charAt(i));
+            if(ht.containsKey(w2.charAt(i))) {
+                count = ht.get(w2.charAt(i));
+                if(count-1 == 0) {
+                    ht.remove(w2.charAt(i));
+                } else {
+                    ht.put(w2.charAt(i), --count);
+                }
             } else {
-                ht.put(w1.charAt(i),1);
+//                System.out.println("Not Matched");
+                isAnagram = false;
+                break;
             }
+
         }
 
-
-
+        isAnagram = ht.isEmpty() ? true : false;
+        System.out.println(isAnagram ? "it is an Anagram": "it is not an Anagram");
 
     }
 }
