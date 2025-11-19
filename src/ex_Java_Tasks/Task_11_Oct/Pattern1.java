eeff19667f3774314268c630b0a9b16d7b443b43
Dhaval <dhavallakhaniofficial@gmail.com>
2025-11-19 23:11:29 +0530

    Code of task of 11 Oct

--- src/ex_Java_Tasks/Task_11_Oct/Pattern1.java
+++ src/ex_Java_Tasks/Task_11_Oct/Pattern1.java
@@ -5,10 +5,11 @@ public class Pattern1 {
         for(int i=1;i<=3;i++) {
 
             for (int j=1;j<=i;j++) {
-                System.out.print("* ");
+                System.out.print("*");
             }
 
             System.out.println();
+
         }
     }
 }
