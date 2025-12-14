931cfa551fc12cf0a6ad1d26964b89dc3d367300
Dhaval <dhavallakhaniofficial@gmail.com>
2025-12-14 23:15:04 +0530

    Code of task of 2 November

--- src/Test2.java
+++ src/Test2.java
@@ -1,2 +1,14 @@
 public class Test2 {
+    public static void main(String[] args) {
+        String s = "I am working on String";
+        String[] tokens = s.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = tokens.length - 1; i >= 0; i--) {
+            result.append(tokens[i]);
+            if (i != 0) result.append(" ");
+        }
+
+        System.out.println(result.toString());
+    }
 }
--- src/ex_30_Collections/Lab109_Lists.java
+++ src/ex_30_Collections/Lab109_Lists.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Lab109_Lists {
     public static void main(String[] args) {
         List li = new ArrayList();
-
         li.add(12);
         li.add("asd");
         li.add(321.321);
