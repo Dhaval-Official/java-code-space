2c3b4f0d602e787b1c0988114331d94080faf626
Dhaval <dhavallakhaniofficial@gmail.com>
2025-12-16 21:47:56 +0530

    Some Extra Template code

---
+++ src/Solution.java
@@ -0,0 +1,25 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Solution {
+
+    public static void main(String[] args) {
+
+        int[][] arr = {{1,3},{2,6},{5,9},{11,15}};
+
+        System.out.println(countAnagramSubstrings("as","sa"));
+    }
+
+
+    public static int[][] merge(int[][] intervals) {
+        // TODO: implement
+        return new int[0][];
+    }
+
+
+    public static int countAnagramSubstrings(String s, String p) {
+        // TODO: implement
+        return 69;
+    }
+
+}
